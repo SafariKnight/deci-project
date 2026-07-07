c2d592b397516631a29a448231c95862cc42b2bc
Kareem Hisham <safariknight2@gmail.com>
2026-07-07 22:36:46 +0300

    cleanup: split prisma declaration

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from "#prisma/client.ts"
 import { PrismaPg } from "@prisma/adapter-pg"
-import "dotenv/config"
 
-export const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! })
-})
+const connectionString = process.env.DATABASE_URL!
+
+const adapter = new PrismaPg({ connectionString })
+
+export const prisma = new PrismaClient({ adapter })
