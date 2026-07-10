e9d33eff4a09e2dbf666c6a9a90cc28ee307c554
Kareem Hisham <safariknight2@gmail.com>
2026-07-10 21:46:19 +0300

    feat: oxlint & oxfmt

--- frontend/src/routes/+page.tsx
+++ frontend/src/routes/+page.tsx
@@ -1,8 +1,10 @@
 import { Link } from "wouter";
 
 export function Root() {
-  return <div>
-    <p>This is the root</p>
-    <Link href="/home">Go to home</Link>
-  </div>
+  return (
+    <div>
+      <p>This is the root</p>
+      <Link href="/home">Go to home</Link>
+    </div>
+  );
 }
