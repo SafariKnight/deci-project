1b4483b8939d652de6d5ac9cf4ae544e1aa2cb13
Kareem Hisham <safariknight2@gmail.com>
2026-08-13 21:36:45 +0300

    a

--- backend/src/vercel.ts
+++ backend/src/vercel.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import type { Request, Response } from "express";
 import app from "./app.js";
 
 export default async function handler(req: Request, res: Response) {
